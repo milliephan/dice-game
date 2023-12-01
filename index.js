61193aebec4e786fca35c8bf23d14f5865693afb
milliephan <thita.phanachet@gmail.com>
2023-12-01 10:29:36 +0700

    done:

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ function rollDie() {
 function assignPicture(diceValue) {
     var dice1Link = "./images/dice" + diceValue[0] + ".png"
     var dice2Link = "./images/dice" + diceValue[1] + ".png"
-    document.querySelector(".img1").setAttribute("src", "dice1Link")
-    document.querySelector(".img2").setAttribute("src", "dice2Link")
+    document.querySelector(".img1").setAttribute("src", dice1Link)
+    document.querySelector(".img2").setAttribute("src", dice2Link)
 }
 
-var diceValue = rollDie();
-assignPicture(diceValue);
+function whoWins(diceValue) {
+    if (diceValue[0] > diceValue[1]) {
+        document.querySelector("h1").textContent = "Player 1 wins!"
+    } else if (diceValue[1] > diceValue[0]) {
+        document.querySelector("h1").textContent = "Player 2 wins!"
+    } else {
+        document.querySelector("h1").textContent = "It's a tie."
+    }
+}
+
+
+if (window.performance.navigation.type !== 0) {
+    var diceValue = rollDie();
+    assignPicture(diceValue);
+    whoWins(diceValue);
+} 
 
